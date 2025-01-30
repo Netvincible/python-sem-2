016ee1effda9d0160f0b207c15a77152f73d2b18
netvincible <netvincible@gmail.com>
2025-01-30 13:59:39 +0530

    lab3 and lab2 q11

---
+++ college-sem-2/lab-3/1.py
@@ -0,0 +1,10 @@
+def vowelcount(string):
+    count=0
+    count+=string.count("a")
+    count+=string.count("e")
+    count+=string.count("i")
+    count+=string.count("o")
+    count+=string.count("u")
+    print(count)
+string=input("enter string: ")
+vowelcount(string)
