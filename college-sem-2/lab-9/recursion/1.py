9c303d44e80a14865c234154c2b8eab8f7afcd11
netvincible <netvincible@gmail.com>
2025-03-20 23:45:22 +0530

    prime fact improved

--- college-sem-2/lab-9/recursion/1.py
+++ college-sem-2/lab-9/recursion/1.py
@@ -1,14 +1,20 @@
-
 l=[]
-def prime_fact(num):
-    factors=0
-    for x in range(2,num): 
-        if num%x==0:
-            prime_fact(x)
-            factors+=1
-    if factors==0:
-        l.append(num)
 
-prime_fact(344)
-l=set(l)
+def prime_fact(num):
+    x=int(num**0.5)
+    if x<=1:
+        return num
+    while(num%x!=0):
+        x-=1
+        if x<=2:
+            return num
+    else:
+        a=prime_fact(x)
+        if a!=None:
+            l.append(a)
+        a=prime_fact(num/x)
+        if a!=None:
+            l.append(a)
+            
+prime_fact(4284)
 print(l)
