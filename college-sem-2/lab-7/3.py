fc9b2c963b8024c0544301e62b08cb8dd93b1354
netvincible <netvincible@gmail.com>
2025-05-01 12:08:37 +0530

    corrected code

--- college-sem-2/lab-7/3.py
+++ college-sem-2/lab-7/3.py
@@ -1,8 +1,9 @@
 def min_max(rec):
-	max_val=max(rec.values())
-	min_val=min(rec.values())
-	print(list(rec.keys())[list(rec.values()).index(max_val)],"has the maximum salary",max_val)
-	print(list(rec.keys())[list(rec.values()).index(min_val)],"has the minimum salary",min_val)
+	for k,v in rec.items():
+		for i in v:
+			min_sal=min(i[1] for i in v)
+			max_sal=max(i[1] for i in v)
+		print(f"{k} has min salary:{min_sal} and max salary:{max_sal}")
 
-rec={(1,4012):1000,(2,2023):500,(3,1065):20000}
+rec={"CSE":((1,2000),(2,3000),(3,1000)),"EC":((1,500),(2,5000),(3,50000)),"ICT":((1,700),(2,500))}
 min_max(rec)
